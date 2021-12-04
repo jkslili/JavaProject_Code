fb35ea10744bc3252f3756a1a47aa397868a887f
jkslili <2604608012@qq.com>
2021-12-04 17:13:51 +0800

    “快速排序”

---
+++ StruCode/选择排序.java
@@ -0,0 +1,34 @@
+package StruCode;
+
+public class 选择排序 {
+    /*
+    function: start min to end max
+     */
+    static public void selectArray(int []arr)
+    {
+        for (int i = 0; i < arr.length-1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                index = arr[index] > arr[j] ? j : index;
+            }
+            if(index==i)
+                continue;
+            arr[i] = arr[i] ^ arr[index];
+            arr[index] = arr[i] ^ arr[index];
+            arr[i] = arr[i] ^ arr[index];
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr={9,2,1,5,3,7,8};
+        selectArray(arr);
+        display(arr);
+    }
+    static void display(int [] arr)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
